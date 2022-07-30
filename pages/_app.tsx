5a57631ee5b8ecdcbe3ee407e4f3aba2ca27722d
Mr. F <functionsandglory@proton.me>
2022-07-30 16:47:06 -0700

    SEO and Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type {FC} from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import type {AppProps} from 'next/app';
 import {CssBaseline, GlobalStyles} from "@mui/material";
 import {ThemeProvider} from '@mui/material/styles';
@@ -19,7 +20,30 @@ const App: FC<AppProps> = ({Component, pageProps}) => {
                 }}/>
                 <CssBaseline/>
                 <Head>
+                    {/* Basic Meta */}
                     <title>Rubber Duck Therapy</title>
+                    <meta name="description"
+                          content="Rubber duck debugging chat bot. Implemented using Eliza. Sometimes just talking about your problem with present you with the solution. Use Rubber Duck Therapy for all your problem solving needs!"/>
+                    <meta name="robots" content="index, follow"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    {/* Open Graph  */}
+                    <meta property="og:url" content="https://rubberducktherapy.app"/>
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:title" content="Rubber Duck Therapy"/>
+                    <meta property="og:description"
+                          content="Rubber duck debugging chat bot. Implemented using Eliza. Sometimes just talking about your problem with present you with the solution. Use Rubber Duck Therapy for all your problem solving needs!"/>
+                    <meta property="og:image"
+                          content="https://www.rubberducktherapy.app/_next/static/media/therapist-rubber-duck.8d5b54bd.png"/>
+                    {/* Twitter Card */}
+                    <meta name="twitter:card" content="summary_large_image"/>
+                    <meta name="twitter:description"
+                          content="Rubber duck debugging chat bot. Implemented using Eliza. Sometimes just talking about your problem with present you with the solution. Use Rubber Duck Therapy for all your problem solving needs!"/>
+                    <meta name="twitter:title" content="Rubber Duck Therapy"/>
+                    <meta name="twitter:site" content="https://rubberducktherapy.app"/>
+                    <meta name="twitter:image"
+                          content="https://www.rubberducktherapy.app/_next/static/media/therapist-rubber-duck.8d5b54bd.png"/>
+                    <meta name="twitter:creator" content="@FuncsAndGlory"/>
+                    {/* Favicon */}
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -27,6 +51,20 @@ const App: FC<AppProps> = ({Component, pageProps}) => {
                     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
                     <meta name="msapplication-TileColor" content="#da532c"/>
                     <meta name="theme-color" content="#ffffff"/>
+                    {/* Google Analytics */}
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-FJM0JW97FQ"
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+    
+                            gtag('config', 'G-FJM0JW97FQ');
+                        `}
+                    </Script>
                 </Head>
                 <Component {...pageProps} />
             </ThemeProvider>
