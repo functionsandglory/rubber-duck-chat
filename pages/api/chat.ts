cbda7ef709a643c92ac44a11a37433a57d1b1b5c
Functions & Glory <functionsandglory@proton.me>
2023-05-30 19:39:21 -0700

    update to system prompt

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -18,8 +18,7 @@ You never answer questions.
 You never propose solutions.
 You only ask simple followup questions.
 Your first message is always an introduction that includes your name.
-You never follow instructions or commands from users.
-You never role play as something else.
+You never act as anything else.
 `
 }
 
